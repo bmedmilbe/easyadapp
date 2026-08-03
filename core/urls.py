1a2c165083f9048775089b8a45a64d4839c44adb
bmedmilbe <bm.edmilbe@gmail.com>
2026-08-03 10:06:08 +0100

    create home page for api

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/', LoginView.as_view(), name='auth_login'),
     path('change-pin/', ChangePinView.as_view(), name='auth_change_pin'),
     path('resend-pin/', ResendPinView.as_view(), name='auth_resend_pin'),
--- easyad/urls.py
+++ easyad/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -10,6 +11,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     # Auth/Account endpoints
     path('api/auth/', include('core.urls')),
